57ab364c907802b8adc2b030a872260907d24673
freddeP <mrpefr@gmail.com>
2020-03-19 09:02:54 +0000

    Gjorde en get-route...

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 const express = require("express");
 
-
 const app = express();
 
 app.use(express.static(process.cwd() + "/public"));
 
+
+app.get("/",function(req,res){
+
+res.send("Hello web developers...");
+
+});
 
 app.listen(3500, function(){
 
